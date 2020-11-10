9ac4c050ed13ace9153702342c301188fdfcf450
jsaarest <39669220+jsaarest@users.noreply.github.com>
2020-11-10 13:03:33 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
-
-const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
     console.log('connected to MongoDB')
   })
