75c9bccbd82d84976676c2adcc1ceed3bfefd3b8
jsaarest <39669220+jsaarest@users.noreply.github.com>
2020-11-10 12:37:49 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
+const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+
 morgan.token('body', (req, res) => JSON.stringify(req.body)); // Use morgan to log request body
 app.use(morgan(':method :url :response-time :body'))
 
@@ -49,8 +50,7 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(result => {
-    res.json(result)
-    mongoose.connection.close()
+    res.json(result.map(person => person.toJSON()))
   })
 })
 
